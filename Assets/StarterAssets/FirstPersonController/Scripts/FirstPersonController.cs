e88382008df01e6dab3cc8c80506d7b2754d8312
Lojin <systemfile36@gmail.com>
2022-04-15 22:55:45 +0900

    충돌판정 개선
    충돌 판정 기즈모 추가, 충돌 판정 방식 개선, 기타 세세한 변수 조정

--- Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
+++ Assets/StarterAssets/FirstPersonController/Scripts/FirstPersonController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
 using UnityEngine.InputSystem;
 #endif
@@ -9,6 +11,9 @@ using UnityEngine.InputSystem;
 
 /* StarterAssets을 변형하였다. 한국어 주석이 붙은 것은 주로 내가 수정한 부분
  * 기존의 에셋 스크립트에 내가 정의한 Player 기능이 들어가 있다.
+ * 
+ * FirstPersonController.cs = Player의 기능을 추가한 파일
+ * StarterAssetsInputs.cs = 입출력 기능을 추가한 파일
  */
 
 namespace StarterAssets
@@ -47,7 +52,7 @@ namespace StarterAssets
 		[Tooltip("Useful for rough ground")]
 		public float GroundedOffset = -0.14f;
 		[Tooltip("The radius of the grounded check. Should match the radius of the CharacterController")]
-		public float GroundedRadius = 0.2f;
+		public float GroundedRadius = 0.3f;
 		[Tooltip("What layers the character uses as ground")]
 		public LayerMask GroundLayers;
 
@@ -55,9 +60,9 @@ namespace StarterAssets
 		[Tooltip("The follow target set in the Cinemachine Virtual Camera that the camera will follow")]
 		public GameObject CinemachineCameraTarget;
 		[Tooltip("How far in degrees can you move the camera up")]
-		public float TopClamp = 90.0f;
+		public float TopClamp = 85.0f;
 		[Tooltip("How far in degrees can you move the camera down")]
-		public float BottomClamp = -90.0f;
+		public float BottomClamp = -85.0f;
 
 		// cinemachine
 		private float _cinemachineTargetPitch;
@@ -84,13 +89,89 @@ namespace StarterAssets
 		//플레이어 판정 범위용 변수
 		[Header("Colliders")]
 		[Tooltip("Width of Player Collision Check")]
-		public float pWidthCol = 0.2f;
+		public static float pWidthCol = 0.3f;
 		[Tooltip("Height Offset of Player Collision Check")]
-		public float pHeightCol = 1.5f;
+		public static float pHeightCol = 1.7f;
 		[Tooltip("Offset for XZ Collision Check, adding to y")]
-		public float pYOffset = 0.5f;
+		public static float pYOffset = 0.08f;
 		[Tooltip("Offset for Side Collision Check, adding to pWidth")]
-		public float pWidthSideOffset = 0.2f;
+		public static float pWidthSideOffset = 0.15f;
+
+		#region 충돌 판정용 열거형, 딕셔너리
+		/// <summary>
+		/// 충돌 판정의 방향과 위치
+		/// </summary>
+		private enum C_Direction
+		{
+			PosX_High,
+			PosX_Low,
+
+			NegX_High,
+			NegX_Low,
+
+			PosZ_High,
+			PosZ_Low,
+
+			NegZ_High,
+			NegZ_Low,
+
+			PosXPosZ_High,
+			PosXPosZ_Low,
+
+			PosXNegZ_High,
+			PosXNegZ_Low,
+
+			NegXPosZ_High,
+			NegXPosZ_Low,
+
+			NegXNegZ_High,
+			NegXNegZ_Low
+		}
+
+		//대각선 방향을 위해 길이에 cos(45도)를 곱함
+		private static float D_pWidthCol = pWidthCol * 0.707f;
+
+		/// <summary>
+		/// 충돌 판정의 플레이어 기준 상대위치
+		/// 이렇게 하면 관리하기 수월해진다. player의 transform.position에 더하면 절대 좌표가 됨
+		/// </summary>
+		private Dictionary<C_Direction, Vector3> pColliders = new Dictionary<C_Direction, Vector3>()
+		{
+			//PosX
+			{C_Direction.PosX_High, new Vector3((pWidthCol + pWidthSideOffset), pHeightCol, 0) },
+			{C_Direction.PosX_Low, new Vector3((pWidthCol + pWidthSideOffset), pYOffset, 0) },
+
+			//PosZ
+			{C_Direction.PosZ_High, new Vector3(0, pHeightCol, (pWidthCol + pWidthSideOffset)) },
+			{C_Direction.PosZ_Low, new Vector3(0, pYOffset, (pWidthCol + pWidthSideOffset)) },
+
+			//NegX
+			{C_Direction.NegX_High, new Vector3(-(pWidthCol + pWidthSideOffset), pHeightCol, 0) },
+			{C_Direction.NegX_Low, new Vector3(-(pWidthCol + pWidthSideOffset), pYOffset, 0) },
+
+			//NegZ
+			{C_Direction.NegZ_High, new Vector3(0, pHeightCol, -(pWidthCol + pWidthSideOffset)) },
+			{C_Direction.NegZ_Low, new Vector3(0, pYOffset, -(pWidthCol + pWidthSideOffset)) },
+
+			//PosXPosZ
+			{C_Direction.PosXPosZ_High, new Vector3(D_pWidthCol + pWidthSideOffset, pHeightCol, D_pWidthCol + pWidthSideOffset) },
+			{C_Direction.PosXPosZ_Low, new Vector3(D_pWidthCol + pWidthSideOffset, pYOffset, D_pWidthCol + pWidthSideOffset) },
+
+			//PosXNegZ
+			{C_Direction.PosXNegZ_High, new Vector3(D_pWidthCol + pWidthSideOffset, pHeightCol, -(D_pWidthCol + pWidthSideOffset)) },
+			{C_Direction.PosXNegZ_Low, new Vector3(D_pWidthCol + pWidthSideOffset, pYOffset, -(D_pWidthCol + pWidthSideOffset)) },
+
+			//NegXPosZ
+			{C_Direction.NegXPosZ_High, new Vector3(-(D_pWidthCol + pWidthSideOffset), pHeightCol, D_pWidthCol + pWidthSideOffset) },
+			{C_Direction.NegXPosZ_Low, new Vector3(-(D_pWidthCol + pWidthSideOffset), pYOffset, D_pWidthCol + pWidthSideOffset) },
+
+			//NegXNegZ
+			{C_Direction.NegXNegZ_High, new Vector3(-(D_pWidthCol + pWidthSideOffset), pHeightCol, -(D_pWidthCol + pWidthSideOffset))  },
+			{C_Direction.NegXNegZ_Low, new Vector3(-(D_pWidthCol + pWidthSideOffset), pYOffset, -(D_pWidthCol + pWidthSideOffset))  }
+
+
+		};
+		#endregion
 
 		//가이드 블럭 참조
 		[Header("Guide Reference")]
@@ -138,6 +219,7 @@ namespace StarterAssets
 			{
 				world = GameObject.Find("World").GetComponent<World>();
 			}
+
 		}
 
 		private void Start()
@@ -167,31 +249,6 @@ namespace StarterAssets
 
 			PlaceAndDestroyBlock();
 
-			/*
-			//스크롤로 블럭을 선택하는 로직
-			if (_input.ScrollAxis != 0)
-			{
-				Debug.Log(_input.ScrollAxis);
-				//스크롤 값에 따라 인덱스 증감
-				if (_input.ScrollAxis > _input.ScrollThreshold)
-					HoldingBlockIndex++;
-				else if(_input.ScrollAxis < -_input.ScrollThreshold)
-					HoldingBlockIndex--;
-
-				//범위 체크해서 순회하게 만듬
-				if(HoldingBlockIndex > (byte)(world.blockTypes.Length - 1))
-				{
-					HoldingBlockIndex = 1;
-				}
-
-				if(HoldingBlockIndex < 1)
-				{
-					HoldingBlockIndex = (byte)(world.blockTypes.Length - 1);
-				}
-
-				
-			}
-			*/
 		}
 
 		private void FixedUpdate()
@@ -329,8 +386,6 @@ namespace StarterAssets
 			placeGuide.gameObject.SetActive(false);
 		}
 
-		//--End--
-
 		private void GroundedCheck()
 		{
 			/*
@@ -356,10 +411,10 @@ namespace StarterAssets
 		/// </summary>
 		private void UpCollision()
 		{
-			if ((world.CheckForVoxel(transform.position.x + pWidthCol, transform.position.y + pHeightCol + 1f, transform.position.z + pWidthCol)
-				|| world.CheckForVoxel(transform.position.x - pWidthCol, transform.position.y + pHeightCol + 1f, transform.position.z - pWidthCol)
-				|| world.CheckForVoxel(transform.position.x - pWidthCol, transform.position.y + pHeightCol + 1f, transform.position.z + pWidthCol)
-				|| world.CheckForVoxel(transform.position.x + pWidthCol, transform.position.y + pHeightCol + 1f, transform.position.z - pWidthCol))
+			if ((world.CheckForVoxel(transform.position.x + pWidthCol, transform.position.y + pHeightCol, transform.position.z + pWidthCol)
+				|| world.CheckForVoxel(transform.position.x - pWidthCol, transform.position.y + pHeightCol, transform.position.z - pWidthCol)
+				|| world.CheckForVoxel(transform.position.x - pWidthCol, transform.position.y + pHeightCol, transform.position.z + pWidthCol)
+				|| world.CheckForVoxel(transform.position.x + pWidthCol, transform.position.y + pHeightCol, transform.position.z - pWidthCol))
 				&& _verticalVelocity >= 0)
 			{
 				//상승 중일때만 적용
@@ -369,7 +424,7 @@ namespace StarterAssets
 			}
 		}
 
-		#region X, Z축 충돌 판정
+		#region X, Z축 충돌 판정 관련 코드
 
 		/// <summary>
 		/// +X방향 충돌 확인
@@ -378,8 +433,8 @@ namespace StarterAssets
 		{
 			get
 			{
-				if (world.CheckForVoxel(transform.position.x + pWidthCol + pWidthSideOffset, transform.position.y + pHeightCol, transform.position.z)
-				|| world.CheckForVoxel(transform.position.x + pWidthCol + pWidthSideOffset, transform.position.y + pYOffset, transform.position.z))
+				if (world.CheckVoxelSolid(pColliders[C_Direction.PosX_High] + transform.position)
+				|| world.CheckVoxelSolid(pColliders[C_Direction.PosX_Low] + transform.position))
 				{
 					return true;
 				}
@@ -397,8 +452,8 @@ namespace StarterAssets
 		{
 			get
 			{
-				if (world.CheckForVoxel(transform.position.x - pWidthCol - pWidthSideOffset, transform.position.y + pHeightCol, transform.position.z)
-				|| world.CheckForVoxel(transform.position.x - pWidthCol - pWidthSideOffset, transform.position.y + pYOffset, transform.position.z))
+				if (world.CheckVoxelSolid(pColliders[C_Direction.NegX_High] + transform.position)
+				|| world.CheckVoxelSolid(pColliders[C_Direction.NegX_Low] + transform.position))
 				{
 					return true;
 
@@ -416,8 +471,8 @@ namespace StarterAssets
 		{
 			get
 			{
-				if (world.CheckForVoxel(transform.position.x, transform.position.y + pHeightCol, transform.position.z + pWidthCol + pWidthSideOffset)
-				|| world.CheckForVoxel(transform.position.x, transform.position.y + pYOffset, transform.position.z + pWidthCol + pWidthSideOffset))
+				if (world.CheckVoxelSolid(pColliders[C_Direction.PosZ_High] + transform.position)
+				|| world.CheckVoxelSolid(pColliders[C_Direction.PosZ_Low] + transform.position))
 				{
 					return true;
 				}
@@ -435,8 +490,8 @@ namespace StarterAssets
 		{
 			get
 			{
-				if (world.CheckForVoxel(transform.position.x, transform.position.y + pHeightCol, transform.position.z - pWidthCol - pWidthSideOffset)
-				|| world.CheckForVoxel(transform.position.x, transform.position.y + pYOffset, transform.position.z - pWidthCol - pWidthSideOffset))
+				if (world.CheckVoxelSolid(pColliders[C_Direction.NegZ_High] + transform.position)
+				|| world.CheckVoxelSolid(pColliders[C_Direction.NegZ_Low] + transform.position))
 				{
 					return true;
 				}
@@ -447,10 +502,89 @@ namespace StarterAssets
 			}
 		}
 
-		
+		/// <summary>
+		/// +X +Z 방향 충돌
+		/// </summary>
+		private bool PosXPosZ
+		{
+			get
+			{
+				if(world.CheckVoxelSolid(pColliders[C_Direction.PosXPosZ_High] + transform.position)
+					|| world.CheckVoxelSolid(pColliders[C_Direction.PosXPosZ_Low] + transform.position))
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// +X -Z 방향 충돌
+		/// </summary>
+		private bool PosXNegZ
+		{
+			get
+			{
+				if (world.CheckVoxelSolid(pColliders[C_Direction.PosXNegZ_High] + transform.position)
+					|| world.CheckVoxelSolid(pColliders[C_Direction.PosXNegZ_Low] + transform.position))
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// -X +Z 방향 충돌
+		/// </summary>
+		private bool NegXPosZ
+		{
+			get
+			{
+				if (world.CheckVoxelSolid(pColliders[C_Direction.NegXPosZ_High] + transform.position)
+					|| world.CheckVoxelSolid(pColliders[C_Direction.NegXPosZ_Low] + transform.position))
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+		/// <summary>
+		/// -X -Z 방향 충돌
+		/// </summary>
+		private bool NegXNegZ
+		{
+			get
+			{
+				if (world.CheckVoxelSolid(pColliders[C_Direction.NegXNegZ_High] + transform.position)
+					|| world.CheckVoxelSolid(pColliders[C_Direction.NegXNegZ_Low] + transform.position))
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
+
+
 
 		#endregion
 
+		/// <summary>
+		/// 중력 반영, 착지 시에는 적용 X
+		/// </summary>
 		private void ApplyGravity()
 		{
 			
@@ -501,6 +635,7 @@ namespace StarterAssets
 			//float speedOffset = 0.1f;
 			//float inputMagnitude = _input.analogMovement ? _input.move.magnitude : 1f;
 
+			#region 가속부 (주석 처리됨)
 			/*
 			 * 이 마인크래프트에서는 필요가 없다.
 			//가속을 담당하는 코드, 
@@ -519,6 +654,7 @@ namespace StarterAssets
 				_speed = targetSpeed;
 			}
 			*/
+			#endregion
 
 			_speed = targetSpeed;
 
@@ -527,8 +663,6 @@ namespace StarterAssets
 
 			// note: Vector2's != operator uses approximation so is not floating point error prone, and is cheaper than magnitude
 			// if there is a move input rotate player when the player is moving
-			
-		
 			if (_input.move != Vector2.zero)
 			{
 				//rotation을 고려해서 x축과 z축 방향을 정함, 즉 세계 기준으로 바꿈
@@ -549,13 +683,18 @@ namespace StarterAssets
 				tempVector.z = 0.0f;
 			}
 			
+			//대각선 충돌
+			if((tempVector.x > 0 && tempVector.z > 0 && PosXPosZ)
+				|| (tempVector.x > 0 && tempVector.z < 0 && PosXNegZ)
+				|| (tempVector.x < 0 && tempVector.z > 0 && NegXPosZ)
+				|| (tempVector.x < 0 && tempVector.z < 0 && NegXNegZ))
+			{
+				tempVector.x = 0.0f;
+				tempVector.z = 0.0f;
+			}
 			
-			//SideCollision(ref tempVector);
-			// move the player
 			//_controller.Move(inputDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
 			transform.Translate(tempVector, Space.World);
-			//Debug.Log("PosX : " + world.CheckForVoxel(transform.position.x + pWidthCol, transform.position.y + pHeightCol - 0.1f, transform.position.z) + "\n" +
-			//	"PosZ : " + world.CheckForVoxel(transform.position.x, transform.position.y + pHeightCol - 0.1f, transform.position.z + pWidthCol));
 		}
 
 		private void Jump()
@@ -628,16 +767,56 @@ namespace StarterAssets
 		//기즈모는 디버그 창에 보이는 것
 		private void OnDrawGizmosSelected()
 		{
+			/*
 			Color transparentGreen = new Color(0.0f, 1.0f, 0.0f, 0.35f);
 			Color transparentRed = new Color(1.0f, 0.0f, 0.0f, 0.35f);
 
 			if (Grounded) Gizmos.color = transparentGreen;
 			else Gizmos.color = transparentRed;
 
-			Gizmos.DrawCube(new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z), new Vector3(pWidthCol, pHeightCol/2.0f, pWidthCol));
+			//Gizmos.DrawCube(new Vector3(transform.position.x, transform.position.y + 1f, transform.position.z), new Vector3(pWidthCol, pHeightCol/2.0f, pWidthCol));
 
 			// when selected, draw a gizmo in the position of, and matching radius of, the grounded collider
 			Gizmos.DrawSphere(new Vector3(transform.position.x, transform.position.y - GroundedOffset, transform.position.z), GroundedRadius);
+			*/
+		}
+
+
+		//충돌판정 가시화를 위한 기즈모
+		private void OnDrawGizmos()
+		{
+
+			Gizmos.color = Color.red;
+
+			//옆면 충돌 체크
+			Gizmos.DrawLine(transform.position + new Vector3(0, pYOffset, 0), pColliders[C_Direction.PosX_Low] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pYOffset, 0), pColliders[C_Direction.PosZ_Low] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pYOffset, 0), pColliders[C_Direction.NegX_Low] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pYOffset, 0), pColliders[C_Direction.NegZ_Low] + transform.position);
+
+			Gizmos.DrawLine(transform.position + new Vector3(0, pYOffset, 0), pColliders[C_Direction.PosXPosZ_Low] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pYOffset, 0), pColliders[C_Direction.PosXNegZ_Low] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pYOffset, 0), pColliders[C_Direction.NegXPosZ_Low] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pYOffset, 0), pColliders[C_Direction.NegXNegZ_Low] + transform.position);
+
+
+			Gizmos.DrawLine(transform.position + new Vector3(0, pHeightCol, 0), pColliders[C_Direction.PosX_High] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pHeightCol, 0), pColliders[C_Direction.PosZ_High] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pHeightCol, 0), pColliders[C_Direction.NegX_High] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pHeightCol, 0), pColliders[C_Direction.NegZ_High] + transform.position);
+
+			Gizmos.DrawLine(transform.position + new Vector3(0, pHeightCol, 0), pColliders[C_Direction.PosXPosZ_High] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pHeightCol, 0), pColliders[C_Direction.PosXNegZ_High] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pHeightCol, 0), pColliders[C_Direction.NegXPosZ_High] + transform.position);
+			Gizmos.DrawLine(transform.position + new Vector3(0, pHeightCol, 0), pColliders[C_Direction.NegXNegZ_High] + transform.position);
+
+
+			//바닥 판정
+			Gizmos.DrawCube(transform.position + new Vector3(0, GroundedOffset, 0), new Vector3(2 * GroundedRadius, 0, 2 * GroundedRadius));
+
+			//머리 판정
+			Gizmos.DrawCube(transform.position + new Vector3(0, pHeightCol, 0), new Vector3(2 * pWidthCol, 0, 2 * pWidthCol));
+
 		}
 	}
 }
